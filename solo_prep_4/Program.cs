8cee102f56e8c57052ad7f6ff201e9add487d120
Glen Meredith <mer16014@byui.edu>
2022-04-27 19:13:00 -0600

    soloprep4

--- solo_prep_4/Program.cs
+++ solo_prep_4/Program.cs
@@ -7,7 +7,38 @@ namespace solo_prep_4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<int> numbers = new List<int>();
+            Console.WriteLine("Enter a list of numbers, type 0 when finished. ");
+            int userNum = 1;
+            while (userNum != 0) {
+                Console.Write("Enter number: ");
+                string userNumStr = Console.ReadLine();
+                userNum = int.Parse(userNumStr);
+                if (userNum != 0) {
+                    numbers.Add(userNum);
+                } else {
+                    break;
+                }
+            }
+            int sum = 0;
+            int average = 0;
+            int max = numbers[1];
+            int min = numbers[1];
+
+            foreach (int number in numbers) {
+                sum += number;
+                if (number < min) {
+                    min = number; 
+                } else if (number > max) {
+                    max = number;
+                }
+            }
+            average = sum / numbers.Count;
+            Console.WriteLine($"The total is {sum}. ");
+            Console.WriteLine($"Max: {max}");
+            Console.WriteLine($"Min: {min}");
+            Console.WriteLine($"Average: {average}");
+
         }
     }
 }
